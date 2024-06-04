2e3f74a78c1a363ee1ddb1ae91bc60931a6422a1
boombog <95608831+boombog@users.noreply.github.com>
2024-06-04 23:57:08 +0300

    FAQ Accordion custom styles + minify

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import {
+  Accordion
+} from '@chakra-ui/react'
+import FAQBlock from "../FAQBlock/FAQBlock";
 
 const FAQ = () => {
   return (
-    <div>FAQ</div>
-  )
-}
+    <div>
+      <Accordion allowToggle>
+        <FAQBlock title={"1. Почему вход на сайт сейчас недоступен?"} text={"Потому что хост еще не оплачен, пожалуйста, подождите"}/>
+        <FAQBlock title={"2. Что случилось с платформой?"} text={"С ней все хорошо, ведутся технические работы, скоро все починим, и вы снова сможете приступать к работе"}/>
+        <FAQBlock title={"3. Как зарегистрироваться?"} text={"Нужно всего лишь на ссылку под названием 'Авторизация', после чего зарегистрироваться, выбрав вариант 'Зарегистрироваться'."}/>
+      </Accordion>
+    </div>
+  );
+};
 
-export default FAQ
+export default FAQ;
---
+++ src/components/FAQBlock/FAQBlock.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from "@chakra-ui/react";
+
+const FAQBlock = ({title, text}) => {
+  return (
+    <AccordionItem bg={"var(--tg-theme-bg-color)"} color={"var(--tg-theme-text-color)"}>
+      <h2>
+        <AccordionButton>
+          <Box as="span" flex="1" textAlign="left" color={"var(--tg-theme-text-color)"}>
+            {title}
+          </Box>
+          <AccordionIcon color={"var(--tg-theme-text-color)"} />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4} bg={"var(--tg-theme-bg-color)"} color={"var(--tg-theme-text-color)"}>
+        {text}
+      </AccordionPanel>
+    </AccordionItem>
+  );
+};
+
+export default FAQBlock;
