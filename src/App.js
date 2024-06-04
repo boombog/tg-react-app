197fb30604409f9f28e02b6b9cb69a31c8e45164
boombog <95608831+boombog@users.noreply.github.com>
2024-06-04 11:02:55 +0300

    eslint changes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import {Route, Routes} from 'react-router-dom'
 import './App.css';
 import MainPage from './components/MainPage/MainPage';
 
-const tg = window.Telegram.WebApp;
 function App() {
   const {tg} = useTelegram();
 
   useEffect(() => {
     tg.ready();
-  }, [])
+  }, [tg])
 
 
   return (
