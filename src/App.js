a55ff2475d93d72461df4cf52afa8969622e50d8
boombog <95608831+boombog@users.noreply.github.com>
2024-06-04 10:45:46 +0300

    upd +new MainPage block v0.1.1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { useEffect } from 'react';
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+// import {Route, Routes} from 'react-router-dom'
 import './App.css';
+import MainPage from './components/MainPage/MainPage';
 
 const tg = window.Telegram.WebApp;
 function App() {
+  const {tg} = useTelegram();
 
   useEffect(() => {
     tg.ready();
   }, [])
 
-  const onClose = () => {
-    
-  }
 
   return (
     <div className="App">
-      work
-
+      <Header />
+      <Routes>
+        <Route index element={<MainPage/>} />
+      </Routes>
     </div>
   );
 }
