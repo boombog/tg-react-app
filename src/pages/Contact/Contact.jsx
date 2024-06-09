4e539aa45e17db44f461005e8284bedcb1b17017
boombog <95608831+boombog@users.noreply.github.com>
2024-06-09 06:40:20 +0300

    fix 4

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,84 +5,96 @@ import {
   Textarea,
   FormErrorMessage,
 } from "@chakra-ui/react";
-import axios from 'axios';
+import axios from "axios";
 import { Field, Form, Formik } from "formik";
 import "./Contact.css";
 import { useTelegram } from "../../hooks/useTelegram";
 import { useEffect, useState, useCallback } from "react";
 
 const Contact = () => {
-
-  const { tg, user, queryId, fetchURL } = useTelegram(); 
-
-  const [message, setMessage] = useState();
-
-  const onChangeMessage = (e) => {
-    setMessage(e.target.value)
-  }
+  const { tg, user, queryId, fetchURL } = useTelegram();
+  const [message, setMessage] = useState("");
 
   const onSendData = useCallback(() => {
     const data = {
       message: message,
       login: user?.username || "testmode",
       userID: user?.id || "111111",
-      queryId: queryId
-    }
-    axios.post(`${fetchURL}/api/sendrequest`, JSON.stringify(data), {
+      queryId: queryId,
+    };
+    axios
+      .post(`${fetchURL}/api/sendrequest`, JSON.stringify(data), {
         headers: {
-          'Content-Type': 'application/json'
-        }
-    }).then(response => {
-        console.log('Успешно отправлено:', response.data);
-    }).catch(error => {
-        console.error('Ошибка отправки:', error);
-    });
-  }, [message, user?.username, user?.id, queryId, fetchURL])
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log("Успешно отправлено:", response.data);
+      })
+      .catch((error) => {
+        console.error("Ошибка отправки:", error);
+      });
+  }, [message, user?.username, user?.id, queryId, fetchURL]);
 
   useEffect(() => {
-    tg.onEvent('MainButtonClicked', onSendData)
+    tg.onEvent("MainButtonClicked", onSendData);
     return () => {
-      tg.offEvent('MainButtonClicked', onSendData)
-    }
-  }, [tg, onSendData])
-
+      tg.offEvent("MainButtonClicked", onSendData);
+    };
+  }, [tg, onSendData]);
 
   function validateMessage(value) {
     let error;
     if (!value) {
-      tg.MainButton.hide()
+      tg.MainButton.hide();
       error = "Поле обязательно для заполнения";
     } else {
-      tg.MainButton.show()
+      tg.MainButton.show();
     }
     return error;
   }
+
   useEffect(() => {
     tg.MainButton.setParams({
-      text: "Отправить данные"
-    })
-  }, [tg])
+      text: "Отправить данные",
+    });
+  }, [tg]);
 
   return (
-    <Formik
-      initialValues={{ message: "" }}
-    >
-      <Form className="MainForm">
-        <Text fontSize="3xl" align="center" color={"var(--tg-theme-text-color)"}>
-          Опишите вашу проблему
-        </Text>
-        <Field name="message" validate={validateMessage}>
-          {({ field, form }) => (
-            <FormControl
-              isInvalid={form.errors.message && form.touched.message}
-            >
-              <FormLabel color={"var(--tg-theme-text-color)"}>В этом сообщении</FormLabel>
-              <Textarea value={message} onChange={onChangeMessage} color={"var(--tg-theme-text-color)"} {...field} placeholder="Сообщение" />
-              <FormErrorMessage>{form.errors.message}</FormErrorMessage>
-            </FormControl>
-          )}
-        </Field>
-      </Form>
+    <Formik initialValues={{ message: "" }} onSubmit={onSendData}>
+      {({ setFieldValue }) => (
+        <Form className="MainForm">
+          <Text
+            fontSize="3xl"
+            align="center"
+            color={"var(--tg-theme-text-color)"}
+          >
+            Опишите вашу проблему
+          </Text>
+          <Field name="message" validate={validateMessage}>
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.message && form.touched.message}
+              >
+                <FormLabel color={"var(--tg-theme-text-color)"}>
+                  Сообщение
+                </FormLabel>
+                <Textarea
+                  {...field}
+                  value={message}
+                  onChange={(e) => {
+                    setMessage(e.target.value);
+                    setFieldValue("message", e.target.value);
+                  }}
+                  color={"var(--tg-theme-text-color)"}
+                  placeholder="Сообщение"
+                />
+                <FormErrorMessage>{form.errors.message}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+        </Form>
+      )}
     </Formik>
   );
 };
