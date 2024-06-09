987610620e30a806cdf0e3a92525ad70085782ad
boombog <95608831+boombog@users.noreply.github.com>
2024-06-09 05:42:20 +0300

    contact backend sendData

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,14 +5,50 @@ import {
   Textarea,
   FormErrorMessage,
 } from "@chakra-ui/react";
+import axios from 'axios';
 import { Field, Form, Formik } from "formik";
 import "./Contact.css";
 import { useTelegram } from "../../hooks/useTelegram";
-import { useEffect } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 const Contact = () => {
 
-  const { tg } = useTelegram()
+  const { tg, user, queryId, fetchURL } = useTelegram(); 
+
+  const [message, setMessage] = useState();
+
+  const onChangeMessage = (e) => {
+    setMessage(e.target.value)
+  }
+
+  const onSendData = useCallback(() => {
+    const login = user?.username || "testmode"
+    const userID = user?.id || "111111"
+    const data = {
+      message,
+      login,
+      userID,
+      queryId
+    }
+    axios.post(`${fetchURL}/api/sendrequest`, data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        console.log('Успешно отправлено:', response.data);
+    }).catch(error => {
+        console.error('Ошибка отправки:', error);
+    });
+  }, [message, user?.username, user?.id, queryId, fetchURL])
+
+  useEffect(() => {
+    tg.onEvent('MainButtonClicked', onSendData)
+    return () => {
+      tg.offEvent('MainButtonClicked', onSendData)
+    }
+  }, [tg, onSendData])
+
+
   function validateMessage(value) {
     let error;
     if (!value) {
@@ -32,9 +68,6 @@ const Contact = () => {
   return (
     <Formik
       initialValues={{ message: "" }}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
     >
       <Form className="MainForm">
         <Text fontSize="3xl" align="center" color={"var(--tg-theme-text-color)"}>
@@ -46,7 +79,7 @@ const Contact = () => {
               isInvalid={form.errors.message && form.touched.message}
             >
               <FormLabel color={"var(--tg-theme-text-color)"}>В этом сообщении</FormLabel>
-              <Textarea color={"var(--tg-theme-text-color)"} {...field} placeholder="Сообщение" />
+              <Textarea value={message} onChange={onChangeMessage} color={"var(--tg-theme-text-color)"} {...field} placeholder="Сообщение" />
               <FormErrorMessage>{form.errors.message}</FormErrorMessage>
             </FormControl>
           )}
